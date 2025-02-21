0c08de6f57aee02e7f43fe4960588c1818d70c97
ltsambo <aung@ltsambo.co.kr>
2025-02-21 08:50:44 +0800

    env changes

--- env/Scripts/activate.bat
+++ env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=D:\2025_Projects\Flutter\yolo_backend\env
+set VIRTUAL_ENV=E:\Django\yolo_backend\env
 
 if not defined PROMPT set PROMPT=$P$G
 
